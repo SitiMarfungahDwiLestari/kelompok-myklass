efa67c62871a1ece657eeb9411f4fbdae50608bb
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2023-04-09 20:52:45 +0700

    commit

--- kelompok myklass/Program.cs
+++ kelompok myklass/Program.cs
@@ -46,10 +46,12 @@ namespace kelompok_myklass
                                         try
                                         {
                                             Console.WriteLine("\nMenu");
-                                            Console.WriteLine("1. Melihat Seluruh Data");
-                                            Console.WriteLine("2. Tambah Data");
-                                            Console.WriteLine("3. Keluar");
-                                            Console.Write("\nEnter your choice (1-3): ");
+                                            Console.WriteLine("1. Melihat Seluruh Data Makanan");
+                                            Console.WriteLine("2. Melihat Seluruh Data Minuman");
+                                            Console.WriteLine("3. Tambah Data Makanan");
+                                            Console.WriteLine("4. Tambah Data Minuman");
+                                            Console.WriteLine("5. Keluar");
+                                            Console.Write("\nEnter your choice (1-5): ");
                                             char ch = Convert.ToChar(Console.ReadLine());
                                             switch (ch)
                                             {
@@ -58,10 +60,18 @@ namespace kelompok_myklass
                                                         Console.Clear();
                                                         Console.WriteLine("DATABASE CAFE\n");
                                                         Console.WriteLine();
-                                                        pr.baca(conn);
+                                                        pr.bacamk(conn);
                                                     }
                                                     break;
                                                 case '2':
+                                                    {
+                                                        Console.Clear();
+                                                        Console.WriteLine("DATABASE CAFE\n");
+                                                        Console.WriteLine();
+                                                        pr.bacamn(conn);
+                                                    }
+                                                    break;
+                                                case '3':
                                                     {
                                                         Console.Clear();
                                                         Console.WriteLine("INPUT DATA MAKANAN\n");
@@ -77,7 +87,7 @@ namespace kelompok_myklass
                                                         string keterangan = Console.ReadLine();
                                                         try
                                                         {
-                                                            pr.insert(id_makanan, nm_makanan, jenis_makanan, harga, keterangan, conn);
+                                                            pr.insertmk(id_makanan, nm_makanan, jenis_makanan, harga, keterangan, conn);
                                                         }
                                                         catch
                                                         {
@@ -85,7 +95,31 @@ namespace kelompok_myklass
                                                         }
                                                     }
                                                     break;
-                                                case '3':
+                                                 case '4':
+                                                    {
+                                                        Console.Clear();
+                                                        Console.WriteLine("INPUT DATA MINUMAN\n");
+                                                        Console.WriteLine("Masukkan Id Minuman : ");
+                                                        string id_minuman = Console.ReadLine();
+                                                        Console.WriteLine("Masukkan Nama Minuman : ");
+                                                        string nm_minuman = Console.ReadLine();
+                                                        Console.WriteLine("Masukkan Jenis Minuman : ");
+                                                        string jenis_minuman = Console.ReadLine();
+                                                        Console.WriteLine("Masukkan Harga Minuman : ");
+                                                        string harga = Console.ReadLine();
+                                                        Console.WriteLine("Masukkan Keterangan : ");
+                                                        string keterangan = Console.ReadLine();
+                                                        try
+                                                        {
+                                                            pr.insertmn(id_minuman, nm_minuman, jenis_minuman, harga, keterangan, conn);
+                                                        }
+                                                        catch
+                                                        {
+                                                            Console.WriteLine("\nAnda tidak memiliki " + "akses untuk menambah data");
+                                                        }
+                                                    }
+                                                    break;
+                                                case '5':
                                                     conn.Close();
                                                     return;
                                                 default:
@@ -115,7 +149,7 @@ namespace kelompok_myklass
                 }
             }
         }
-        public void baca(SqlConnection con)
+        public void bacamk(SqlConnection con)
         {
             SqlCommand cmd = new SqlCommand("Select * from tb_makanan", con);
             SqlDataReader r = cmd.ExecuteReader();
@@ -129,7 +163,21 @@ namespace kelompok_myklass
             }
             r.Close();
         }
-        public void insert(string id_makanan, string nm_makanan, string jenis_makanan, string harga, string keterangan, SqlConnection con)
+        public void bacamn(SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("Select * from tb_minuman", con);
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                for (int i = 0; i < r.FieldCount; i++)
+                {
+                    Console.WriteLine(r.GetValue(i));
+                }
+                Console.WriteLine();
+            }
+            r.Close();
+        }
+        public void insertmk(string id_makanan, string nm_makanan, string jenis_makanan, string harga, string keterangan, SqlConnection con)
         {
             string str = "";
             str = "insert into tb_makanan (id_makanan, nm_makanan, jenis_makanan, harga, keterangan)" + "values (@id, @nama, @jenis, @harganya, @ket)";
@@ -145,5 +193,20 @@ namespace kelompok_myklass
             Console.WriteLine("Data Berhasil Ditambahkan");
         }
 
+        public void insertmn(string id_minuman, string nm_minuman, string jenis_minuman, string harga, string keterangan, SqlConnection con)
+        {
+            string str = "";
+            str = "insert into tb_minuman (id_minuman, nm_minuman, jenis_minuman, harga, keterangan)" + "values (@id, @nama, @jenis, @harganya, @ket)";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.Add(new SqlParameter("id", id_minuman));
+            cmd.Parameters.Add(new SqlParameter("nama", nm_minuman));
+            cmd.Parameters.Add(new SqlParameter("jenis", jenis_minuman));
+            cmd.Parameters.Add(new SqlParameter("harganya", harga));
+            cmd.Parameters.Add(new SqlParameter("ket", keterangan));
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Berhasil Ditambahkan ");
+        }
     }
 }
